3da039cef1ff379e64ae1a8e3423cf3432a339b5
glaubermlira <glauberdevmlira@gmail.com>
2020-12-14 06:12:06 -0300

    second commit

--- js/main.js
+++ js/main.js
@@ -74,11 +74,63 @@
 
 //DATE;
 
-var day = new Date();
+// var day = new Date();
 
 // console.log(day);
 // console.log(day.getDay());
 // console.log(day.getMonth()+1);  //como a contagem se inicia no 0, devemos acrescentar +1 para informar o mês correto;
 // console.log(day.getHours());
 // console.log(day.getMinutes());
-console.log("Hoje é " + day.getDate() + "/" + (day.getMonth()+1) + " de " + day.getFullYear());
+// console.log("Hoje é " + day.getDate() + "/" + (day.getMonth()+1) + " de " + day.getFullYear());
+
+// FUNCTION;
+
+// function somaNumber (n1, n2){
+//     return n1 + n2;
+// }
+
+// console.log(somaNumber(1,5));
+
+// function validarDate (age){
+//     if(age >= 18){
+//         return true;
+//     } else {
+//         return false
+//     }
+
+// }
+
+// var validar = prompt("Informe sua idade");
+// console.log(validarDate(validar));
+
+//MANIPULANDO ELEMENTOS DA PÁGINA;
+
+// function insertData (data){
+//     var data = alert("Seus dados foram enviado com sucesso");
+//     return data;
+// }
+
+
+function returnMessage (){
+    document.getElementById("message").innerHTML = "Seus dados foram carregados com sucesso!";  
+    
+    setTimeout(function(){
+        window.location.href = "http://127.0.0.1:5500/index.html";
+    }, 2000)
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+
+
+
+
+// setTimeout(function(){
+//     alert("Ola mundo!");
+//         //your code to be executed after 1 seconds
+// },delay);
+
+
+// window.setTimeout('funcao()', intervalo_em_milisegundos);
+
